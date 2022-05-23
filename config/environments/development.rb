24d6424090cace72e9a059868ed652db16f78afc
Prashanttech <prashant@techwinners.in>
2022-05-23 11:32:36 +0530

    latest changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -82,6 +82,7 @@ config.action_mailer.default_url_options = { :host => "localhost:3000" }
 
 
 
+
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
 address:              'smtp.gmail.com',
